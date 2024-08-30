6c026616020ddddd690f706566314bf278f5a8b5
Sandra Rudeloff <srudeloff@outlook.com>
2024-08-30 12:55:44 +0200

    Quartz sync: Aug 30, 2024, 12:55 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -30,8 +30,9 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
+    Component.MobileOnly(Component.Explorer({ title: "Navigation", useSavedState: false })),
     Component.DesktopOnly(Component.TableOfContents()),
+    Component.Graph(),
     Component.Backlinks(),
   ],
 }
